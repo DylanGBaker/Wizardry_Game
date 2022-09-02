b919f6cfb26e63815fe6f79740cdeca23aaacdc1
Skaduvee <2093671@students.wits.ac.za>
2022-09-02 18:28:51 +0200

    Worked on enemy script

--- Wizardry/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Wizardry/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rb;
     public Transform Player;
     public bool canMove;
     public float MoveSpeed;
+    [SerializeField] private Vector3 MoveDirection;
 
     private void Start()
     {
         canMove = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.LookAt(Player);
+        Vector3 Direction = (Player.position - transform.position).normalized;
+        MoveDirection = Direction;
+    }
+
+    void FixedUpdate()
+    {
+        //transform.LookAt(Player);
+        LookAtPlayer(MoveDirection);
 
         if (canMove)
         {
-            transform.position = Vector3.MoveTowards(transform.position, Player.position, MoveSpeed * Time.deltaTime);
+            MoveTowardsPlayer(MoveDirection);
         }
+    }
+
+    private void MoveTowardsPlayer(Vector3 movedirection)
+    {
+        //transform.position = (transform.position) + (movedirection * MoveSpeed *Time.deltaTime);
+        rb.velocity = new Vector3(movedirection.x, movedirection.y, movedirection.z) * MoveSpeed;
+    }
+
+    private void LookAtPlayer(Vector3 movedirection)
+    {
+        float Theta = Mathf.Atan2(movedirection.z, movedirection.x) * Mathf.Rad2Deg;
+        Quaternion DeltaAngle = Quaternion.Euler(rb.rotation.x, -Theta, rb.rotation.z);
+        rb.MoveRotation(DeltaAngle); 
     }
 }
