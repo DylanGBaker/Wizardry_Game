bfa8c6c51ca94cc3d7c5596d3156efa3b8d509ba
Skaduvee <2093671@students.wits.ac.za>
2022-09-03 02:46:54 +0200

    Made enemy look at player

--- Wizardry/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Wizardry/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
-    public Transform Player;
-    public bool canMove;
-    public float MoveSpeed;
+    [SerializeField] private Transform Player;
+    [SerializeField] private bool canMove;
+    [SerializeField] private float MoveSpeed;
     [SerializeField] private Vector3 MoveDirection;
 
     private void Start()
@@ -23,25 +23,18 @@ public class EnemyMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        //transform.LookAt(Player);
-        LookAtPlayer(MoveDirection);
-
+        LookAtPlayer();
         if (canMove)
-        {
             MoveTowardsPlayer(MoveDirection);
-        }
     }
 
     private void MoveTowardsPlayer(Vector3 movedirection)
     {
-        //transform.position = (transform.position) + (movedirection * MoveSpeed *Time.deltaTime);
         rb.velocity = new Vector3(movedirection.x, movedirection.y, movedirection.z) * MoveSpeed;
     }
 
-    private void LookAtPlayer(Vector3 movedirection)
+    private void LookAtPlayer()
     {
-        float Theta = Mathf.Atan2(movedirection.z, movedirection.x) * Mathf.Rad2Deg;
-        Quaternion DeltaAngle = Quaternion.Euler(rb.rotation.x, -Theta, rb.rotation.z);
-        rb.MoveRotation(DeltaAngle); 
+        transform.LookAt(Player);
     }
 }
