0c5c7332d8761294360cafc4390a876b89145f70
Skaduvee <2093671@students.wits.ac.za>
2022-09-03 02:49:04 +0200

    Update PlayerMovement.cs
    Cleaned up code

--- Wizardry/Assets/Scripts/Player/PlayerMovement.cs
+++ Wizardry/Assets/Scripts/Player/PlayerMovement.cs
@@ -34,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer(Vector3 playermovementinput)
     {
-        rb.velocity = (transform.right * playermovementinput.x * m_MoveSpeed * Time.deltaTime) + (transform.forward * playermovementinput.z * m_MoveSpeed * Time.deltaTime); 
+        rb.velocity = ((transform.right * playermovementinput.x) + (transform.forward * playermovementinput.z)) * m_MoveSpeed * Time.deltaTime;
     }
 
     private void RotatePlayer(Vector2 playermouseinput)
