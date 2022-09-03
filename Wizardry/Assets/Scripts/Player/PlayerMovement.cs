a364fd56ae551039aaa1f538736b25659bf675b1
Skaduvee <2093671@students.wits.ac.za>
2022-09-03 23:03:02 +0200

    Made code more neat and player prefab

--- Wizardry/Assets/Scripts/Player/PlayerMovement.cs
+++ Wizardry/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,60 +4,60 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Vector2 m_PlayerMovementInput;
-    [SerializeField] private Vector2 m_PlayerMouseInput;
+    [SerializeField] private Vector2 m_playerMovementInput;
+    [SerializeField] private Vector2 m_playerMouseInput;
 
-    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Rigidbody m_rigidBody;
     [SerializeField] private Camera m_Camera;
-    [SerializeField] private Transform m_PlayerTransform;
-    [SerializeField] private KeyCode m_JumpKey;
+    [SerializeField] private KeyCode m_jumpKey;
 
-    [SerializeField] private float m_MoveSpeed, m_Sensitivity, m_xRot, m_yRot, m_JumpSpeed;
+    [SerializeField] private float m_moveSpeed, m_Sensitivity, m_xRot, m_yRot, m_jumpSpeed;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        rb = this.GetComponent<Rigidbody>();
-        m_Camera.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        m_Camera.transform.rotation = Quaternion.Euler(0, 0, 0);
         transform.Rotate(Vector3.zero);    
     }
 
     private void Update()
     {
-        PlayerInput();
-        RotatePlayer(m_PlayerMouseInput);
+        m_playerMovementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        m_playerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        CameraFollow();
+        RotatePlayer(m_playerMouseInput);
         Jump();
     }
 
     private void FixedUpdate()
     {
-        MovePlayer(m_PlayerMovementInput);
+        MovePlayer(m_playerMovementInput);
     }
 
-    private void PlayerInput()
-    {
-        m_PlayerMovementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        m_PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X") * m_Sensitivity * Time.deltaTime, Input.GetAxis("Mouse Y") * m_Sensitivity * Time.deltaTime);
-    }
     private void MovePlayer(Vector3 playermovementinput)
     {
-        Vector3 moveDirection = (transform.right * playermovementinput.x * m_MoveSpeed) + (transform.forward * playermovementinput.y * m_MoveSpeed);
-        moveDirection.y = rb.velocity.y;
-        rb.velocity = moveDirection;
-    }
-
-    private void Jump()
-    {
-        if (Input.GetKeyDown(m_JumpKey))
-            rb.AddForce(transform.up * m_JumpSpeed, ForceMode.Impulse);
+        Vector3 moveDirection = (m_moveSpeed * playermovementinput.x * transform.right) + (m_moveSpeed * playermovementinput.y * transform.forward);
+        moveDirection.y = m_rigidBody.velocity.y;
+        m_rigidBody.velocity = moveDirection;
     }
 
     private void RotatePlayer(Vector2 playermouseinput)
     {
-        m_xRot -= playermouseinput.y;
-        m_yRot += playermouseinput.x;
+        m_xRot -= playermouseinput.y * m_Sensitivity * Time.deltaTime;
+        m_yRot += playermouseinput.x * m_Sensitivity * Time.deltaTime;
         m_xRot = Mathf.Clamp(m_xRot, -90f, 90f);
         m_Camera.transform.rotation = Quaternion.Euler(m_xRot, m_yRot, 0f);
-        m_PlayerTransform.rotation = Quaternion.Euler(0, m_yRot, 0);
+        transform.rotation = Quaternion.Euler(0, m_yRot, 0);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(m_jumpKey))
+            m_rigidBody.AddForce(transform.up * m_jumpSpeed, ForceMode.Impulse);
+    }
+
+    private void CameraFollow()
+    {
+        m_Camera.transform.position = transform.position;
     }
 }
